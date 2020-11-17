cb09a03df089708ad4e1b5c5f0b5280112880d18
Luca Silverentand <luca@appcompany.io>
2020-11-17 18:43:12 +0100

    packaged

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,8 @@ async function run() {
     const github_token = core.getInput('GITHUB_TOKEN')
     const context = github.context
     if (context.payload.pull_request == null) {
-        core.setFailed('No pull request found.')
-        return
+      core.setFailed('No pull request found.')
+      return
     }
     const pull_request_number = context.payload.pull_request.number
     const octokit = github.getOctokit(github_token)
