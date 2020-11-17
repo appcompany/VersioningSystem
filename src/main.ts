2ad83a1c82df8b00853b11003685f7f48139e625
Luca Silverentand <luca@appcompany.io>
2020-11-17 18:51:48 +0100

    attempt to fix the github token

--- src/main.ts
+++ src/main.ts
@@ -3,19 +3,21 @@ import * as github from '@actions/github'
 
 async function run() {
   try {
-    const github_token = core.getInput('GITHUB_TOKEN')
     const context = github.context
     if (context.payload.pull_request == null) {
       core.setFailed('No pull request found.')
       return
     }
+    
     const pull_request_number = context.payload.pull_request.number
-    const octokit = github.getOctokit(github_token)
+    const octokit = github.getOctokit(core.getInput('GITHUB_TOKEN') ?? process.env.GITHUB_TOKEN)
+
     octokit.issues.createComment({
       ...context.repo,
       issue_number: pull_request_number,
       body: 'this is a test message.'
     })
+    
   } catch (error) {
     core.setFailed(error.message)
   }
